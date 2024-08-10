d61dd8f8cb455ff17bfe3b17cd465aecb6999d61
RMamedbekov <mamedbekovruslan@yandex.ru>
2024-08-10 11:39:50 +0300

    add total counter

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,36 @@
-import { Component } from '../core/Component';
-import { Form } from './Form';
-import { List } from './List';
-import { ListItem } from './ListItem';
+import { Component } from "../core/Component";
+import { Form } from "./Form";
+import { List } from "./List";
+import { ListItem } from "./ListItem";
 
 export class App extends Component {
   setup(props) {
-    this.$rootElement = document.createElement('div');
-    this.$rootElement.className = 'app';
+    this.state = { total: 0, donates: [] };
+    this.$rootElement = document.createElement("div");
+    this.$rootElement.className = "app";
 
-    // ...
-    
+    // Как-то не очень
+    this.$h1 = document.createElement("h1");
+    this.$h1.className = "total-amount";
+    this.$h1.textContent = "Итого: $ ";
+    // Как-то не очень
+
+    // Как-то не очень
+    this.$total = document.createElement("span");
+    this.$total.textContent = this.state.total;
+
+    // Как-то не очень
+    this.$h1.appendChild(this.$total);
+    // Как-то не очень
+
+    this.$rootElement.appendChild(this.$h1);
+
     const donateForm = new Form();
     this.$rootElement.appendChild(donateForm.$rootElement);
     const donateList = new List();
     this.$rootElement.appendChild(donateList.$rootElement);
   }
-  
+
   onItemCreate(amount) {
     // ...
   }
