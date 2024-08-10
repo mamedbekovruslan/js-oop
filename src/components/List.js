4bc512923ee6556cb4b4fd97a1552a5cf53e9f0a
RMamedbekov <mamedbekovruslan@yandex.ru>
2024-08-10 12:16:27 +0300

    add some logic to List component

--- src/components/List.js
+++ src/components/List.js
@@ -1,10 +1,19 @@
-import { Component } from '../core/Component';
+import { Component } from "../core/Component";
 
 export class List extends Component {
   setup() {
-    this.$rootElement = document.createElement('div');
-    this.$rootElement.className = 'donates-container';
+    this.$rootElement = document.createElement("div");
+    this.$rootElement.className = "donates-container";
 
+    this.$listContainer = document.createElement("div");
+    this.$listContainer.className = "donates-container";
+
+    this.$h2 = document.createElement("h2");
+    this.$h2.className = "donates-container__title";
+    this.$h2.textContent = "Список донатов";
+
+    this.$listContainer.appendChild(this.$h2);
+    this.$rootElement.appendChild(this.$listContainer);
     // ...
   }
 
